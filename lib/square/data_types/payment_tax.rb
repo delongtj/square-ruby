3ca7c64731499a638e09831a929061dfeb987bb1
Brian McAllister <brian@brianmcallister.com>
2015-11-12 13:45:08 -0500

    Remove enum checking.

--- lib/square/data_types/payment_tax.rb
+++ lib/square/data_types/payment_tax.rb
@@ -13,15 +13,11 @@ module Square
       property :rate
 
       # Whether the tax is an ADDITIVE tax or an INCLUSIVE tax.
-      property :inclusion_type, required: true, coerce: -> (value) { self.thing(value) }
+      property :inclusion_type # Fee.InclusionType
 
       # The ID of the tax, if available. Taxes applied in older versions of
       # Square Register might not have an ID.
       property :fee_id
-
-      def self.thing(value)
-        ['ADDITIVE', 'INCLUSIVE'].include?(value) ? value : nil
-      end
     end
   end
 end
