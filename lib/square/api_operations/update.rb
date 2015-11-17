a3ddd1080eca44e12db2bb0c4ea54b2ff69f0f36
Brian McAllister <brian@brianmcallister.com>
2015-11-17 17:52:32 -0500

    Create a sane call signature for update.

--- lib/square/api_operations/update.rb
+++ lib/square/api_operations/update.rb
@@ -3,15 +3,31 @@ module Square
     module Update
       # Update a resource.
       #
-      # @param params [Hash] Update data.
-      def update(params, id, parent_id = nil)
-        if parent_id
-          path = File.join(@nested_under, parent_id, @endpoint_base, id)
-        else
-          path = File.join(@endpoint_base, id)
+      # @param id [String] ID of the resource to update.
+      # @param parent_id [String] ID of the 'parent' to update. Optional.
+      # @param params [Hash] Payload. Optional.
+      def update(*args)
+        # Extract args.
+        case args.count
+          when 3
+            id, parent_id, params = args
+          when 2
+            id, params = args
+          else
+            raise StandardError.new('Unknown number of arguments.')
         end
 
-        response = Square.put(path, params)
+        # Build the endpoint.
+        endpoint = File.join([@nested_under, parent_id, @endpoint_base, id].compact)
+
+        request = {
+          method: 'PUT',
+          endpoint: endpoint,
+          params: params
+        }
+
+        response = Square.make_request(request)
+        response = JSON.parse(response)
         @data_type.new(response)
       end
     end
