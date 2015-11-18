ab16a87fc09e732a885877a689ec765be385e34e
Brian McAllister <brian@brianmcallister.com>
2015-11-18 16:31:30 -0500

    Add specs for the for Square::DataType.

---
+++ spec/square/data_type_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Square::DataType do
+  it 'should be a Hashie::Dash' do
+    data = Square::DataType.new
+
+    expect(data).to be_a Hashie::Dash
+    expect(Square::DataType.included_modules).to include(
+      Hashie::Extensions::Dash::Coercion,
+      Hashie::Extensions::IndifferentAccess
+    )
+  end
+end
