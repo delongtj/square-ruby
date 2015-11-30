24ad576a4d1a306606361f32dd650f4ced0dee9f
Brian McAllister <brian@brianmcallister.com>
2015-11-30 16:27:13 -0500

    Configure webmock to not break with codeclimate maybe.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'square'
 require 'ap'
 require 'webmock/rspec'
 
+# https://github.com/codeclimate/ruby-test-reporter#webmocknetconnectnotallowederror
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
+
 Square.access_token = 'whatever man'
 
 Dir["./spec/support/**/*.rb"].each {|f| require f}
